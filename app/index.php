c0769b3e95e3b06ee283420d3eaf6e591d928dda
vinhkosd <vinh8c059@gmail.com>
2022-01-07 19:10:07 +0700

    remove routes suck

--- app/index.php
+++ app/index.php
@@ -3,7 +3,6 @@ session_start();
 require __DIR__.'/../vendor/autoload.php';
 include 'config.php';
 include 'global.php';
-include 'routes.php';
 include 'function.php';
 
 foreach (glob(__DIR__."/../models/*.php") as $filename)
